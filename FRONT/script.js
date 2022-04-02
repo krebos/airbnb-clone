30195005f257fc156a51d3b7b96ed74a83cb4d2e
krebos <ozon9608@naver.com>
2022-04-02 17:20:52 +0900

    initial commit

---
+++ FRONT/script.js
@@ -0,0 +1,7 @@
+let btn1= document.querySelector('#1');
+let btn2= document.querySelector('#2');
+let btn3= document.querySelector('#3');
+
+btn1.addEventListener('click', () => btnPrimary.style.backgroundColor-'')
+btn2.addEventListener('click', () => )
+btn3.addEventListener('click', () => )
